c13c4a792b0588efe9463facb348a8f6e9738d52
olivier-sonrel <o.sonrel@it-students.fr>
2022-03-30 15:47:45 +0200

    init du recycleview on road

--- app/src/main/java/com/olivier/todo/MainActivity.java
+++ app/src/main/java/com/olivier/todo/MainActivity.java
@@ -2,6 +2,8 @@ package com.olivier.todo;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
@@ -10,12 +12,20 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tvTodos;
     Context context;
+
+    private static final String TODOLIST_KEY = "TODOLIST_KEY";
+    private List<Todo>  todos = new ArrayList<>();
+
+    private RecyclerView recyclerView;
+    private TodoAdapter todoAdapter;
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -45,23 +55,34 @@ public class MainActivity extends AppCompatActivity {
         TodoDAO todoDAO = new TodoDAO(context);
         List<Todo> todos = todoDAO.list();
 
-        String text = "";
+        TodoAdapter todoAdapter = new TodoAdapter(todos);
+
+/*        String text = "";
 
         for( Todo todo : todos){
             text += String.format(" %s / %s \n", todo.getName(), todo.getUrgency());
         }
 
-        tvTodos.setText(text);
+        tvTodos.setText(text);*/
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.recyclerview_main);
+
+        // tvTodos = findViewById(R.id.tvTodos);
+        recyclerView = findViewById(R.id.tvTodos);/* TODO on call pas le bon main je pense ou c le tvTodos qui est pas du bon type dans les init... */
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+        todoAdapter = new TodoAdapter(todos);
+
 
         context = getApplicationContext();
 
-        tvTodos = findViewById(R.id.tvTodos);
+
 
     }
 }
